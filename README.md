54829770c4c8084408441f02afc56068b2ae470e
Hari36020 <harikrishna.sangakari@walkingtree.tech>
2020-08-24 20:19:29 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,14 +1,73 @@
 # pro_data_table
 
-A new Flutter package project.
+A flutter plugin for a custom datatable.
 
-## Getting Started
+## Features:
+* Delete the data rows
+* Theming the grid
+* Export the data in csv file
+* Save Changes
 
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
+### Getting Started
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+
+<img src="https://github.com/abhilashahyd/pro_dataTable/blob/master/assets/images/2020-08-21%20(1).jpg" width="200">
+<img src="https://github.com/abhilashahyd/pro_dataTable/blob/master/assets/images/2020-08-21.jpg" width="200">
+<img src="https://github.com/abhilashahyd/pro_dataTable/blob/master/assets/images/2020-08-23%20(1).jpg" width="200">
+<img src="https://github.com/abhilashahyd/pro_dataTable/blob/master/assets/images/2020-08-23.jpg" width="200">
+
+#### Properties
+
+```final Key key;   //A key for the widget``` 
+
+
+```List<dynamic> tblRow;   //The Table Row data```
+
+> **Example:**  final tblRow = [   <br />
+>             {                      <br />
+>               'firstName': "HariKrishna",  <br />
+>              'lastName': "S",          <br />
+>               'id': 2001,            <br />
+>               'email': "harikrishna.s@walkingtree.com"   <br />
+>             },                       <br />
+>             ];                       <br />
+
+
+
+``` List<dynamic> tblHdr;   //The Table Header column data,The object must contain below params. ``` <br />
+ ``` name ```<br />
+``` num  ```<br />
+``` col ```<br />
+``` flag ```<br />
+``` edit ```<br />
+
+>**Example:** List<dynamic> tblHdr = [ <br />
+>             {                        <br />
+>             'name': 'First Name',  &nbsp; // column name   <br />
+>              'num': true,        &nbsp; // If the column is number column true else false  <br />
+>               'col': 'firstName',  &nbsp; // column id                                <br />
+>                "flag" : false,    &nbsp;// if the column has filter then true else false    <br />
+>                "edit" : true,    &nbsp;// if the column is editable  then true else false    <br />
+>                },                <br />                   
+>          ];                                        
+          
+          
+          
+```String title;     //The Table Title```
+
+
+
+```int selectedSort;   //Pass the default sorted column index```
+
+
+
+```bool sort;         //Default ascending sort true/false```
+
+
+##### Usage:
+
+>ProDataTable(<br />
+>     tblRow: tblRow, <br />
+>      tblHdr: tblHdr, <br />
+>      title: 'The Data Table Demo', <br />
+>    ); <br />
--- lib/pro_data_table.dart
+++ lib/pro_data_table.dart
@@ -1,5 +1,6 @@
 library pro_data_table;
 
+
 // library pro_dataTable;
 
 import 'dart:collection';
@@ -11,10 +12,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class ProDataTable extends StatefulWidget {
-  ///A key for the Widget
+  //A key for the Widget
   final Key key;
   //The Table Row data
   List<dynamic> tblRow;
@@ -52,21 +52,25 @@ class _ProDataTableState extends State<ProDataTable> {
   final lastNameController = TextEditingController();
   bool _sort;
   int _selectedSort;
+  // bool flag = true;
+
   String dropdownValue;
 
   List _rowData;
 
   void initState() {
     super.initState();
-    _rowDatas = widget.tblRow;
+    _rowDatas = widget.tblRow;      //assigning the row data passed to local list
     _rowData = _rowDatas;
-    _sort = widget.sort;
-    _selectedSort = widget.selectedSort;
-    getFilteredCols();
-    onSortColumn(_selectedSort, _sort);
+    _sort = widget.sort;             // assingning the sort true/false to local variable
+    _selectedSort = widget.selectedSort;      // assigning the initial sort column index
+    getFilteredCols();                 // Call the `getFilteredCols` callback function
+    onSortColumn(_selectedSort, _sort);   // Call the `onSortColumn` callback function
     // selectedData = [];
   }
 
+
+// adding the filterable columns to a list
   List<dynamic> getFilteredCols() {
     _filterCols = widget.tblHdr.where((i) => i['filter'] == true).toList();
     print('_filterCols');
@@ -74,11 +78,13 @@ class _ProDataTableState extends State<ProDataTable> {
     // return _filterCols;
   }
 
-  void launchURL(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    }
-  }
+  // void launchURL(String url) async {
+  //   if (await canLaunch(url)) {
+  //     await launch(url);
+  //   }
+  // }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -87,10 +93,11 @@ class _ProDataTableState extends State<ProDataTable> {
         IconButton(
           icon: Icon(Icons.delete),
           onPressed: () {
+            //delete operation on row data with the selected items
             setState(
-              () {
+                  () {
                 _selectedData.forEach(
-                  (element) {
+                      (element) {
                     _rowData.remove(element);
                   },
                 );
@@ -102,7 +109,7 @@ class _ProDataTableState extends State<ProDataTable> {
         IconButton(
           icon: Icon(Icons.share),
           onPressed: () {
-            getCsv(widget.tblHdr, _rowData);
+            getCsv(widget.tblHdr, _rowData); // Call the `onSortColumn` callback function
           },
         ),
       ]),
@@ -111,13 +118,14 @@ class _ProDataTableState extends State<ProDataTable> {
           Expanded(
             child: tableBody(
               context,
-            ),
+            ),                              // Call the `tableBody` callback function
           ),
         ],
       ),
     );
   }
 
+  //This will get the all distinct records  for filter dropdown
   List<dynamic> getDistinct(columnName) {
     _distinctIds =
         LinkedHashSet<String>.from(_rowData.map((e) => e[columnName])).toList();
@@ -125,126 +133,133 @@ class _ProDataTableState extends State<ProDataTable> {
   }
 
   SingleChildScrollView tableBody(BuildContext ctx) {
+    //Filtering data based on the  selected values from dropdown
+    // if selected values are empty show all the data else filtered data
     _rowDatas = _selectedValues.length > 0
         ? searchSelected(
-            _selectedValues,
-          )
+      _selectedValues,
+    )
         : _rowData;
 
+    //Returns the datatable widget
     return SingleChildScrollView(
       scrollDirection: Axis.vertical,
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: DataTable(
           dataRowHeight: 50,
-          dividerThickness: 0,
+          dividerThickness: 5,
           sortAscending: _sort,
           sortColumnIndex: _selectedSort,
           showCheckboxColumn: true,
-          columns: widget.tblHdr
+          columns: widget.tblHdr                          // Mapping the header list which passed as a param
               .map((element) => DataColumn(
-                    label: Container(
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      //  color: Colors.green,
-                      child: Row(
-                        children: [
-                          Text(element['name']),
-                          element['filter'] == true
-                              ? PopupMenuButton(
-                                  itemBuilder: (_) =>
-                                      getDistinct(element['columnName'])
-                                          .map((option) {
-                                        return PopupMenuItem<String>(
-                                            child: Row(
-                                              children: [
-                                                Icon(
-                                                  _selectedValues
-                                                          .contains(option)
-                                                      ? Icons
-                                                          .radio_button_checked
-                                                      : Icons
-                                                          .radio_button_unchecked,
-                                                  color: _selectedValues
-                                                          .contains(option)
-                                                      ? Colors.green
-                                                      : Colors.grey,
-                                                  size: 15,
-                                                ),
-                                                SizedBox(width: 10),
-                                                Text(option)
-                                              ],
-                                            ),
-                                            value: option);
-                                      }).toList(),
-                                  onSelected: (option) {
-                                    setState(() {
-                                      if (_selectedValues.contains(option))
-                                        _selectedValues.remove(option);
-                                      else
-                                        _selectedValues.add(option);
-                                    });
-                                    print(_selectedValues);
-                                  })
-                              : SizedBox()
-                        ],
-                      ),
-                    ),
-                    onSort: (columnIndex, ascending) {
-                      setState(() {
-                        _sort = !_sort;
-                        _selectedSort = columnIndex;
-                      });
-                      onSortColumn(columnIndex, ascending);
-                    },
-                  ))
+            label: Container(
+              width: 150,
+              height: 60,
+              //  color: Colors.green,
+              child: Row(
+                children: [
+                  Text(element['name']),
+                  element['filter'] == true
+                      ? PopupMenuButton(
+                      itemBuilder: (_) =>
+                          getDistinct(element['columnName'])
+                              .map((option) {
+                            return PopupMenuItem<String>(
+                                child: Row(
+                                  children: [
+                                    Icon(                    //if  the the selected values contain  the iteration,will show green radio button.
+                                      _selectedValues
+                                          .contains(option)
+                                          ? Icons
+                                          .radio_button_checked
+                                          : Icons
+                                          .radio_button_unchecked,
+                                      color: _selectedValues
+                                          .contains(option)
+                                          ? Colors.blue
+                                          : Colors.grey,
+                                      size: 15,
+                                    ),
+                                    SizedBox(width: 10),
+                                    Text(option)
+                                  ],
+                                ),
+                                value: option);
+                          }).toList(),
+                      onSelected: (option) {
+                        setState(() {
+                          if (_selectedValues.contains(option)) //check and uncheck the radio button on selected
+                            _selectedValues.remove(option);
+                          else
+                            _selectedValues.add(option);
+                        });
+                        print(_selectedValues);
+                      })
+                      : SizedBox()
+                ],
+              ),
+            ),
+            onSort: (columnIndex, ascending) {
+              setState(() {
+                _sort = !_sort;
+                _selectedSort = columnIndex;
+              });
+              onSortColumn(columnIndex, ascending);
+            },
+          ))
               .toList(),
           rows: _rowDatas
               .map(
                 (_row) => DataRow.byIndex(
-                  index: _rowDatas.indexOf(_row),
-                  // key: ValueKey(user.rollNo),
-                  selected: _selectedData.contains(_row),
-                  color: (_rowDatas.indexOf(_row) % 2) == 0
-                      ? MaterialStateProperty.all<Color>(
-                          Colors.green.withOpacity(0.08))
-                      : MaterialStateProperty.all<Color>(Colors.white),
-                  onSelectChanged: (b) {
-                    print("Onselect");
-                    onSelectedRow(b, _row);
+              index: _rowDatas.indexOf(_row),
+              // key: ValueKey(user.rollNo),
+              selected: _selectedData.contains(_row),
+              color: (_rowDatas.indexOf(_row) % 2) == 0
+                  ? MaterialStateProperty.all<Color>(
+                  Colors.blueAccent.withOpacity(0.08))
+                  : MaterialStateProperty.all<Color>(Colors.white),
+              onSelectChanged: (b) {
+                print("Onselect");
+                onSelectedRow(b, _row);
+              },
+              cells: widget.tblHdr
+                  .map(
+                    (element) => (element['edit'] == true)
+                    ? DataCell(
+                  getCell(
+                    _row[element['columnName']],
+                    element['columnName'],
+                    _rowDatas.indexOf(_row),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      selectedName = _row[element['columnName']];
+                    });
+                    print(
+                        'Toggle ${_row[element['columnName']]}');
                   },
-                  cells: widget.tblHdr
-                      .map(
-                        (element) => (element['edit'] == true)
-                            ? DataCell(
-                                getCell(
-                                  _row[element['columnName']],
-                                  element['columnName'],
-                                  _rowDatas.indexOf(_row),
-                                ),
-                                onTap: () {
-                                  setState(() {
-                                    selectedName = _row[element['columnName']];
-                                  });
-                                  print(
-                                      'Toggle ${_row[element['columnName']]}');
-                                },
-                              )
-                            : DataCell(
-                                getTextCell(
-                                  //'lastName',
-                                  _row[element['columnName']].toString(),
-                                ),
-                              ),
-                      )
-                      .toList(),
-                   ),
+                )
+                    : DataCell(
+                  getTextCell(
+                    //'lastName',
+                    _row[element['columnName']].toString(),
+                  ),
+                ),
               )
+                  .toList(),
+            ),
+          )
               .toList(),
         ),
       ),
     );
   }
+
+
+//passing the initial sort index and sortable true/false
+  //sort operation on columns for ascending and descending
 
   onSortColumn(int columnIndex, bool ascending) {
     print('on sort column');
@@ -265,7 +280,7 @@ class _ProDataTableState extends State<ProDataTable> {
     } else {
       if (ascending) {
         Comparator<dynamic> idComparator =
-            (a, b) => a[columnName].compareTo(b[columnName]);
+            (a, b) => a[columnName].compareTo(b[columnName]); //comparing two columns
         _rowDatas.sort(idComparator);
       } else {
         Comparator<dynamic> idComparator =
@@ -275,6 +290,7 @@ class _ProDataTableState extends State<ProDataTable> {
     }
   }
 
+  //All Selected rows will be added to the  '_selectedData' list.
   onSelectedRow(bool selected, dynamic data) async {
     setState(() {
       if (selected) {
@@ -286,6 +302,7 @@ class _ProDataTableState extends State<ProDataTable> {
     print(_selectedData);
   }
 
+//Updates the edited cell with latest value
   updateName(index, colName) {
     print('In');
     print(editNameController.text);
@@ -298,49 +315,51 @@ class _ProDataTableState extends State<ProDataTable> {
 
     editNameController.text = '';
   }
-
+// Returns Text data cell
   Widget getTextCell(String _rowName) {
     return Text(_rowName);
   }
+// Returns Text data or if edit is true returns a textformfield
 
   Widget getCell(String _rowName, String colName, int index) {
     return (_rowName != selectedName)
         ? Text(_rowName)
         : Container(
-            width: 100,
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.grey, width: 0.2)),
-                    ),
-                    controller: editNameController,
-                    onSaved: (newValue) => editNameController.text = newValue,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    updateName(index, colName);
-                  },
-                  padding: new EdgeInsets.all(2.0),
-                  iconSize: 20,
-                  icon: Icon(Icons.save, color: Colors.black),
-                ),
-              ],
+      width: 100,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderSide:
+                    BorderSide(color: Colors.grey, width: 0.2)),
+              ),
+              controller: editNameController,
+              onSaved: (newValue) => editNameController.text = newValue,
             ),
-          );
+          ),
+          IconButton(
+            onPressed: () {
+              updateName(index, colName);
+            },
+            padding: new EdgeInsets.all(2.0),
+            iconSize: 20,
+            icon: Icon(Icons.save, color: Colors.black),
+          ),
+        ],
+      ),
+    );
   }
 
+  //Returns the filtered data
   searchSelected(List<String> selectedValues) {
     List<dynamic> newDataList = [];
 
     _rowData.forEach(
-      (x) => {
+          (x) => {
         _filterCols.forEach(
-          (element) {
+              (element) {
             if (selectedValues.contains(x[element['columnName']])) {
               if (!newDataList.contains(x)) {
                 newDataList.add(x);
@@ -353,6 +372,8 @@ class _ProDataTableState extends State<ProDataTable> {
     return newDataList;
   }
 
+
+  //Exporting the List data as csv file
   getCsv(associateList, tblRow) async {
     //create an element rows of type list of list. All the above data set are stored in associate list
 //Let associate be a model class with attributes name,gender and age and associateList be a list of associate model class.
@@ -365,7 +386,8 @@ class _ProDataTableState extends State<ProDataTable> {
       for (int j = 0; j < associateList.length; j++) {
         row.add(tblRow[i][associateList[j]['columnName']]);
       }
-          print(row.toString());
+
+      print(row.toString());
       rows.add(row);
     }
 
@@ -391,9 +413,10 @@ class _ProDataTableState extends State<ProDataTable> {
 // convert rows to String and write as csv file
 
       String csv = const ListToCsvConverter().convert(rows);
+      print(csv);
+      print(file);
       await myFile.writeAsString(csv);
-      launchURL(file + ".csv");
+      // launchURL(file + ".csv");
     }
   }
 }
-
