1169865d639011e833486170775bdeb4ba13d367
Hari36020 <harikrishna.sangakari@walkingtree.tech>
2020-08-31 17:33:17 +0530

    Example included

--- lib/pro_data_table.dart
+++ lib/pro_data_table.dart
@@ -89,8 +89,10 @@ class _ProDataTableState extends State<ProDataTable> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(widget.title), actions: [
+
+      appBar: AppBar(key: ValueKey('abc'),title: Text(widget.title), actions: [
         IconButton(
+          key:  ValueKey('abcd'),
           icon: Icon(Icons.delete),
           onPressed: () {
             //delete operation on row data with the selected items
@@ -107,6 +109,7 @@ class _ProDataTableState extends State<ProDataTable> {
           },
         ),
         IconButton(
+          key:  ValueKey('abcde'),
           icon: Icon(Icons.share),
           onPressed: () {
             getCsv(widget.tblHdr, _rowData); // Call the `onSortColumn` callback function
--- test/pro_data_table_test.dart
+++ test/pro_data_table_test.dart
@@ -1,13 +1,37 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:pro_data_table/pro_data_table.dart';
 
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  testWidgets('datatable test', (WidgetTester tester)async{
+
+    await tester.pumpWidget(ProDataTable(   tblRow : [
+    {
+    'firstName': "Shubham",
+    'lastName': "B",
+    'id': 2001,
+    'email': "shubham.b@walkingtree.tech"
+    }],tblHdr: [
+      {
+      'name': 'First Name',
+      'numeric': false,
+      'columnName': 'firstName',
+      'filter': false,
+      'edit': true
+      },]));
+
+    final singleChildScrollViewFinder  = find.byKey(ValueKey("abc"));
+    expect(singleChildScrollViewFinder, findsOneWidget);
+
+    final containerFinder  = find.byKey(ValueKey("abcd"));
+    expect(containerFinder, findsOneWidget);
+
+    final listViewFinder  = find.byKey(ValueKey("abcde"));
+    expect(listViewFinder, findsOneWidget);
+
+
   });
+
 }
+
