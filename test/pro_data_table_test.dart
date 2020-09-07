eec158e8ef0d086d606f7ce02b72ff16aecfbfe3
abhilashahyd <abhilasha.sinha@walkingtree.tech>
2020-09-07 09:33:58 +0530

    Test script updates

--- test/pro_data_table_test.dart
+++ test/pro_data_table_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:pro_data_table/pro_data_table.dart';
@@ -5,19 +6,58 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('pro_data_table', (WidgetTester tester)async{
+  testWidgets('pro_data_table', (WidgetTester tester) async {
+    final tblRow = [
+      {
+        'firstName': "Test",
+        'lastName': "B",
+        'id': 2001,
+        'email': "test@walkingtree.tech"
+      },
+    ];
+    final tblHdr = [
+      {
+        'name': 'First Name',
+        'numeric': false,
+        'columnName': 'firstName',
+        'filter': false,
+        'edit': true
+      },
+      // {
+      //   'name': 'Last Name',
+      //   'numeric': false,
+      //   'columnName': 'lastName',
+      //   'filter': true,
+      // },
+      // {
+      //   'name': 'Id',
+      //   'numeric': true,
+      //   'columnName': 'id',
+      //   'filter': false,
+      // },
+      // {
+      //   'name': 'Email Id',
+      //   'numeric': false,
+      //   'columnName': 'email',
+      //   'filter': true,
+      //   'edit': true
+      // },
+    ];
+    await tester.pumpWidget(MaterialApp(
+      home: ProDataTable(
+        tblRow: tblRow,
+        tblHdr: tblHdr,
+        title: 'The Data Table Demo',
+      ),
+    ));
 
-    await tester.pumpWidget(ProDataTable());
-
-    final singleChildScrollViewFinder  = find.byKey(ValueKey("singlechildkey"));
+    final singleChildScrollViewFinder = find.byKey(ValueKey("singlechildkey"));
     expect(singleChildScrollViewFinder, findsOneWidget);
 
-    final datatableFinder  = find.byKey(ValueKey("datatablekey"));
+    final datatableFinder = find.byKey(ValueKey("datatablekey"));
     expect(datatableFinder, findsOneWidget);
 
-    final appbarFinder  = find.byKey(ValueKey("appbarkey"));
+    final appbarFinder = find.byKey(ValueKey("appbarkey"));
     expect(appbarFinder, findsOneWidget);
-
   });
-
 }
